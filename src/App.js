58b8f6fa1c78d0b94320b8e55550164324f1be04
SomendraZ <somendra.swaroop@kalvium.community>
2023-01-10 16:44:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Morehook from './components/Morehook';
+
+function App() {
+  return (
+    <div className="App">
+      <Morehook/>
+    </div>
+  );
+}
+
+export default App;
